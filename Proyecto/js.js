6be4b39d1ff17e7d3ab4ad47ba4dba7768967649
Jesus Benavides <jdbg231@gmail.com>
2021-09-03 20:44:38 -0400

    creé archivo js con sus funciones

---
+++ Proyecto/js.js
@@ -0,0 +1,32 @@
+let newPokemonToAdd = prompt("quieres agregar un pokemon?");
+
+class Pokemon {
+    constructor(nombre, tipo, nivel) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.tipo = tipo;
+    }
+
+
+    toString(){
+        return `{nombre: ${this.nombre}, nivel: ${this.nivel}, tipo: ${this.tipo}}`
+    }
+}
+
+const pokemones = []
+
+while (newPokemonToAdd == "si") {
+    let nombreP = prompt("Nombre Pokemon")
+    let tipoP = prompt("Tipo de Pokemon")
+    let nivelP = prompt("Nivel del pokemon")
+    const pokemon = new Pokemon(nombreP, tipoP, nivelP);
+    pokemones.push(pokemon);
+    newPokemonToAdd = prompt("quieres agregar un pokemon?");
+}
+
+// let infoP = "Tu pokemon es " + nombreP + ", es del tipo " + tipoP + " y su nivel es " + nivelP;
+
+// alert(infoP)
+
+
+alert("estos son todos los pokemones =>" + pokemones.toString())
