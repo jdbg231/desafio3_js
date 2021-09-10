c090074b58e5e2b04d96132257df8e4ec9caf4be
Jesus Benavides <jdbg231@gmail.com>
2021-09-10 20:19:25 -0300

    creación archivo pokedex.html e integración de elementos gráficos con css

--- Proyecto/js.js
+++ Proyecto/js.js
@@ -1,39 +1,41 @@
-let newPokemonToAdd = prompt("quieres agregar un pokemon?");
-
-class Pokemon {
-    constructor(nombre, tipo, nivel) {
-        this.nombre = nombre;
-        this.nivel = nivel;
-        this.tipo = tipo;
-    }
-
-
-    toString(){
-        return `{nombre: ${this.nombre}, nivel: ${this.nivel}, tipo: ${this.tipo}}`
-    }
-}
-
-const pokemones = []
-
-while (newPokemonToAdd == "si") {
-    let nombreP = prompt("Nombre Pokemon")
-    let tipoP = prompt("Tipo de Pokemon")
-    let nivelP = prompt("Nivel del pokemon")
-    const pokemon = new Pokemon(nombreP, tipoP, nivelP);
-    pokemones.push(pokemon);
-    newPokemonToAdd = prompt("quieres agregar un pokemon?");
-}
-
-// let infoP = "Tu pokemon es " + nombreP + ", es del tipo " + tipoP + " y su nivel es " + nivelP;
-
-// alert(infoP)
-
-
-alert("estos son todos los pokemones =>" + pokemones.toString())
+// let newPokemonToAdd = prompt("quieres agregar un pokemon?");
 //
-const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+// class Pokemon {
+//     constructor(nombre, tipo, nivel) {
+//         this.nombre = nombre;
+//         this.nivel = nivel;
+//         this.tipo = tipo;
+//     }
 //
-// for (const pokemon of pokemones) {
-//     guardarLocal(pokemon.nombre, JSON.stringify(pokemon));
+//
+//     toString(){
+//         return `{nombre: ${this.nombre}, nivel: ${this.nivel}, tipo: ${this.tipo}}`
+//     }
 // }
-guardarLocal("listaPokemones", JSON.stringify(pokemones));
+//
+// const pokemones = []
+//
+// while (newPokemonToAdd === "si") {
+//     let nombreP = prompt("Nombre Pokemon")
+//     let tipoP = prompt("Tipo de Pokemon")
+//     let nivelP = prompt("Nivel del pokemon")
+//     const pokemon = new Pokemon(nombreP, tipoP, nivelP);
+//     pokemones.push(pokemon);
+//     newPokemonToAdd = prompt("quieres agregar un pokemon?");
+// }
+//
+// // let infoP = "Tu pokemon es " + nombreP + ", es del tipo " + tipoP + " y su nivel es " + nivelP;
+//
+// // alert(infoP)
+//
+//
+// alert("estos son todos los pokemones =>" + pokemones.toString())
+//
+// pokemones.forEach(value => console.log(`pokemon saved => ${value}`))
+// //
+// const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+// //
+// // for (const pokemon of pokemones) {
+// //     guardarLocal(pokemon.nombre, JSON.stringify(pokemon));
+// // }
+// guardarLocal("listaPokemones", JSON.stringify(pokemones));
